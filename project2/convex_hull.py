eb8149692ea7e3e61033c5f2aaf46533e0d922e0
AldenKim <aldenkim.ut@gmail.com>
2024-09-25 19:29:11 -0600

    Add code to brute force when we have only two points

--- project2/convex_hull.py
+++ project2/convex_hull.py
@@ -1,6 +1,8 @@
 # Uncomment this line to import some functions that can help
 # you debug your algorithm
 # from plotting import draw_line, draw_hull, circle_point
+
+#Code for our data structure
 class Node:
     def __init__(self, point: tuple[float, float]):
         self.point = point
@@ -21,11 +23,12 @@ class DoublyLinkedList:
             self.head = newNode
             self.tail = newNode
         else:
-            self.tail = self.head.prev
-            self.tail.next = newNode
-            newNode.prev = newNode
+            tail = self.tail
+            tail.next = newNode
+            newNode.prev = tail
             newNode.next = self.head
             self.head.prev = newNode
+            self.tail = newNode
 
     def remove(self, node: Node):
         if node == self.head and self.head == self.tail:
@@ -35,14 +38,59 @@ class DoublyLinkedList:
             node.next.prev = node.prev
             if node == self.head:
                 self.head = node.next
+#Code for our data structure
 
-def brute_force_points():
+def find_line(p1: tuple[float, float], p2: tuple[float, float]):
+    x1, y1 = p1
+    x2, y2 = p2
 
+    if x1 == x2:
+        return None, x1
 
+    m = (y2 - y1) / (x2 - x1)
+    b = y1 - m * x1
+    return m, b
+
+def brute_force_points(points: list[tuple[float, float]]):
+    hull = []
+    n = len(points)
+    for i in range(n):
+        for j in range(n):
+            p1 = points[i]
+            p2 = points[j]
+
+            m, b = find_line(p1, p2)
+
+            count_above = 0
+            count_below = 0
+
+            for point in points:
+                if m is None:
+                    if point[0] > b:
+                        count_above += 1
+                    elif point[0] < b:
+                        count_below += 1
+                else:
+                    y_line = m * point[0] + b
+                    if point[1] > y_line:
+                        count_above += 1
+                    elif point[1] < y_line:
+                        count_below += 1
+
+            if count_above == 0 or count_below == 0:
+                if p1 not in hull:
+                    hull.append(p1)
+                if p2 not in hull:
+                    hull.append(p2)
+
+    return hull
 
 def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]:
     """Return the subset of provided points that define the convex hull"""
     if len(points) < 3:
         #brute force it
+        return brute_force_points(points)
+
+    points.sort()
 
     return []
