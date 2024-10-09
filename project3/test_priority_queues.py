bf3c2bad31f62e504a8de56d942d3ce14749d498
AldenKim <aldenkim.ut@gmail.com>
2024-10-08 18:56:28 -0600

    Updating tests to new ones

--- project3/test_priority_queues.py
+++ project3/test_priority_queues.py
@@ -1,24 +1,28 @@
-from priority_queues import ArrayPQ, HeapPQ
-
+from priorityqueues import ArrayPQ, HeapPQ
+from math import inf as INF
 
 def test_pqs():
     items = list(range(10))
+
     array_pq = ArrayPQ(items)
-    heap_pq = HeapPQ(items)
+    for i in range(10):
+        array_pq.update_priority(i, INF)
 
-    array_pq.set_priority(7, 7)
-    heap_pq.set_priority(7, 7)
+    #heap_pq = HeapPQ(items)
 
-    assert next(array_pq) == 7
-    assert next(heap_pq) == 7
+    array_pq.update_priority(7, 7)
+    #heap_pq.set_priority(7, 7)
 
-    array_pq.set_priority(3, 3)
-    array_pq.set_priority(5, 1)
-    heap_pq.set_priority(3, 3)
-    heap_pq.set_priority(5, 1)
+    assert array_pq.pop_min() == 7
+    #assert next(heap_pq) == 7
 
-    assert next(array_pq) == 5
-    assert next(array_pq) == 3
+    array_pq.update_priority(3, 3)
+    array_pq.update_priority(5, 1)
+    #heap_pq.set_priority(3, 3)
+    #heap_pq.set_priority(5, 1)
 
-    assert next(heap_pq) == 5
-    assert next(heap_pq) == 3
+    assert array_pq.pop_min() == 5
+    assert array_pq.pop_min() == 3
+
+    #assert next(heap_pq) == 5
+    #assert next(heap_pq) == 3
