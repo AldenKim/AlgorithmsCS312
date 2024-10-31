7000df5a09eb7f998a72f5b98df44546969c2d00
AldenKim <aldenkim.ut@gmail.com>
2024-10-30 18:07:01 -0600

    Getting alignment costs

--- project4/alignment.py
+++ project4/alignment.py
@@ -1,12 +1,5 @@
-def align(
-        seq1: str,
-        seq2: str,
-        match_award=-3,
-        indel_penalty=5,
-        sub_penalty=1,
-        banded_width=-1,
-        gap='-'
-) -> tuple[float, str | None, str | None]:
+def align(seq1: str, seq2: str, match_award=-3, indel_penalty=5,
+        sub_penalty=1, banded_width=-1,gap='-') -> tuple[float, str | None, str | None]:
     """
         Align seq1 against seq2 using Needleman-Wunsch
         Put seq1 on left (j) and seq2 on top (i)
@@ -20,4 +13,32 @@ def align(
         :param gap: the character to use to represent gaps in the alignment strings
         :return: alignment cost, alignment 1, alignment 2
     """
+    left_size = len(seq1)
+    top_size = len(seq2)
+
+    matrix = {(0, 0): 0}
+
+    for i in range(1, left_size+1):
+        matrix[(i, 0)] = i * indel_penalty
+    for j in range(1, top_size+1):
+        matrix[(0, j)] = j * indel_penalty
+
+    for i in range(1, left_size + 1):
+        for j in range(1, top_size + 1):
+            if seq1[i - 1] == seq2[j - 1]:
+                match = matrix[(i - 1, j - 1)] + match_award
+            else:  # Substitution
+                match = matrix[(i - 1, j - 1)] + sub_penalty
+
+            above = matrix[(i - 1, j)] + indel_penalty
+            left = matrix[(i, j - 1)] + indel_penalty
+
+            matrix[(i, j)] = min(match, above, left)
+
+    alignment_cost = matrix[(left_size, top_size)]
+
+
+
+    return alignment_cost, None, None
+
 
